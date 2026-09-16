0efeab345959ce4a42d4c5a4b2a10fdb73be0b18
applestoree <applestoremalaysiaa@gmail.com>
2026-09-17 01:25:14 +0800

    Disable Konsta UI wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App as KonstaApp } from 'konsta/react';
 import App from '../App.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <KonstaApp theme="material">
-      <App />
-    </KonstaApp>
+    <App />
   </React.StrictMode>,
 );
