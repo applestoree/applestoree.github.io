c5bb179b7243077a6586ffeb2209b6ad259aef87
applestoree <applestoremalaysiaa@gmail.com>
2026-09-18 00:37:58 +0800

    Add shared header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+      <Link to="/home">Apple Store Malaysia</Link>
+    </header>
+  )
+}
+
+export default Header
